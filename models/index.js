740d856c3d3527f86bae7d58766d1b5ba6d365c2
zachfrosty4986 <zachjames0114@gmail.com>
2024-06-10 16:25:28 -0400

    Updated index.js model with statements

--- models/index.js
+++ models/index.js
@@ -5,12 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  as: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  as: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tag_id',
+});
 
 module.exports = {
   Product,
