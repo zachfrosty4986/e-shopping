a21e983ded4c9e268f0b5a11ecba769aeff41551
zachfrosty4986 <zachjames0114@gmail.com>
2024-06-10 17:00:41 -0400

    Updated Tag.js model by defining collumns

--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,19 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
